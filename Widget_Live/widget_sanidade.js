e36c9c49d4fe82754208e623b39d5e5e4d695a2f
Rafael Callegari <86686780+rafaelcallegari@users.noreply.github.com>
2025-02-23 22:43:07 -0300

    Widget da live sincronizando

--- Widget_Live/widget_sanidade.js
+++ Widget_Live/widget_sanidade.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 
 const supabaseUrl = 'https://tflmxppqxtarzbdwulqc.supabase.co';
-const supabaseKey = 'SUA_SUPABASE_KEY';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmbG14cHBxeHRhcnpiZHd1bHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyODE3NzgsImV4cCI6MjA1NTg1Nzc3OH0.STkMH-gbbjeqg0qmIV4i081Rv6fzzQYX06jKkzKbRBw';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Obtém o nome do jogador da URL
